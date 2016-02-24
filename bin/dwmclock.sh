9c25a3a777fc0b5609c4006d19a2753943bcbd2c
shuall <shualloret@gmail.com>
2016-02-24 17:10:09 -0500

    dwmclock newness
    specify with super unsecure status line
    not fault tolerant

--- bin/dwmclock.sh
+++ bin/dwmclock.sh
@@ -1,12 +1,55 @@
 #!/bin/sh
+
+# helper functions
+MPD_CUR="/dev/shm/mpd_current"
+MPD_USE=0
+
+mpd_current () {
+	while :; do
+		mpc current -f '[[%artist% - ]%title%]' > $MPD_CUR
+		mpc idle
+	done
+}
+
+
+# status getters
+Swifi () {
+	# requires some fancy stuff in netctl
+	# to put name into /tmp/current-network
+	cat /tmp/current-network
+}
+
+Smute () {
+    if [ -n "$(amixer sget Master | grep off)" ]; then
+        echo "_"
+	else
+		echo "#"
+	fi
+}
+
+Sbat () {
+	acpi | awk '{if ($3 != "Discharging,") { printf "+" } printf "%d%%\n", $4}'
+}
+
+Sdate () {
+	date +"%D %R:%S"
+}
+
+Smpd () {
+	if [ $MPD_USE = 0 ]; then
+		mpd_current &
+		MPD_USE=1
+	fi
+
+	cat $MPD_CUR
+}
+
+#super duper luper
 while true
 do
-    if [ -n "$(amixer sget Master | grep off)" ]; then
-        mute="M "
-    else
-        mute=""
-    fi
-    xsetroot -name "$(cat /tmp/current-network) ${mute}$(acpi | awk '{printf "%d%%\n", $4}') $(date +"%D %R:%S")"
+	stat=$(echo "$@" | sed 's/\([^ ]*\)/$(S\1)/g')
+	stat="printf '%s\\n' \"$stat\""
+	xsetroot -name "$(eval $stat)"
     sleep 1s;
 done
 
