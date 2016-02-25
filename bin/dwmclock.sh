9261f663026c76d3ca7baef4c9a73950574f7d67
shuall <EMAIL@SITE.BIZ>
2016-02-25 16:53:46 -0500

    dwmclock.sh, mpd works mo betta

--- bin/dwmclock.sh
+++ bin/dwmclock.sh
@@ -1,29 +1,29 @@
 #!/bin/sh
 
 # helper functions
+NET_CUR="/tmp/current-network"
 MPD_CUR="/dev/shm/mpd_current"
-MPD_USE=0
-
-mpd_current () {
+Impd () {
 	while :; do
-		mpc current -f '[[%artist% - ]%title%]' > $MPD_CUR
-		mpc idle
-	done
+		mpc >/dev/null 2>&1
+		while [ $? -eq 0 ] ; do
+			mpc current -f '[[%artist% - ]%title%]' > "$MPD_CUR"
+			mpc idle >/dev/null
+		done
+	sleep 5
+	done &
 }
-
 
 # status getters
 Swifi () {
 	# requires some fancy stuff in netctl
 	# to put name into /tmp/current-network
-	cat /tmp/current-network
+	cat "$NET_CUR" 
 }
 
 Smute () {
     if [ -n "$(amixer sget Master | grep off)" ]; then
-        echo "_"
-	else
-		echo "#"
+        echo "M"
 	fi
 }
 
@@ -36,20 +36,32 @@ Sdate () {
 }
 
 Smpd () {
-	if [ $MPD_USE = 0 ]; then
-		mpd_current &
-		MPD_USE=1
-	fi
-
-	cat $MPD_CUR
+	[ -r "$MPD_CUR" ] && cat "$MPD_CUR" || return 0
 }
 
 #super duper luper
+for a in $@; do
+	i="I$a"
+	(type $i >/dev/null 2>&1) && $i
+done
+
 while true
 do
-	stat=$(echo "$@" | sed 's/\([^ ]*\)/$(S\1)/g')
-	stat="printf '%s\\n' \"$stat\""
-	xsetroot -name "$(eval $stat)"
+	# this could be done with a sed and eval
+	# replacing all a with $(S$a) but this seems better
+	# I haven't noticed a slowdown except when I used
+	# type "S$a" so I don't use that
+	stat=""
+	for a in $@; do
+		s="S$a"
+		sret="$($s 2>/dev/null)"
+		if [ $? -eq 0 ]; then
+			[ -n "$sret" ] && stat="${stat} ${sret}"
+		else
+			stat="${stat} $a"
+		fi
+	done
+	xsetroot -name "$stat"
     sleep 1s;
 done
 
