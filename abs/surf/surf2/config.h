0060e9c68f253d5c7804e9a78311eadc09e2f00d
shuall <shualloret@gmail.com>
2016-10-26 17:45:45 -0400

    abs:surf2: media clicky
    middle click opens new tab
    MOD+middle click opens new window

--- abs/surf/surf2/config.h
+++ abs/surf/surf2/config.h
@@ -90,7 +90,7 @@ static WebKitFindOptions findopts = WEBKIT_FIND_OPTIONS_CASE_INSENSITIVE |
 /* VIDEOPLAY(URI) */
 #define VIDEOPLAY(u) {\
         .v = (const char *[]){ "/bin/sh", "-c", \
-             "mpv --really-quiet \"$0\" $([ -n \"$1\" ] && echo \"--wid $1\")", u, embedid,  NULL \
+             "mpv --really-quiet \"$0\" $([ -n \"$1\" ] && echo \"--wid $1\")", u, (a->i)?"":embedid,  NULL \
         } \
 }
 
@@ -174,7 +174,8 @@ static Button buttons[] = {
 	{ OnLink,       MODKEY,         1,      clicknewwindow, { .b = 1 },     1 },
 	{ OnAny,        0,              8,      clicknavigate,  { .i = -1 },    1 },
 	{ OnAny,        0,              9,      clicknavigate,  { .i = +1 },    1 },
-	{ OnMedia,      MODKEY,         1,      clickexternplayer, { 0 },       1 },
+	{ OnMedia,      MODKEY,         1,      clickexternplayer, { 1 },       1 },
+	{ OnMedia,      0,              2,      clickexternplayer, { 0 },       1 },
 };
 
 static SearchEngine searchengines[] = {
