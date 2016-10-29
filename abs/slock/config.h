86b652a461cbf849ff58b315f8d4cdf7ee8a71ce
shuall <shualloret@gmail.com>
2016-10-29 12:01:00 -0400

    abs:slock: colors, and new version

--- abs/slock/config.h
+++ abs/slock/config.h
@@ -1,2 +1,12 @@
-#define COLOR1 "black"
-#define COLOR2 "#FF9900"
+/* user and group to drop privileges to */
+static const char *user  = "nobody";
+static const char *group = "nobody";
+
+static const char *colorname[NUMCOLS] = {
+	"black",     /* after initialization */
+	"#FF9900",   /* during input */
+	"#CC4400",   /* wrong password */
+};
+
+/* treat a cleared input like a wrong password */
+static const int failonclear = 1;
