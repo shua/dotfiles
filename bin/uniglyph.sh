0560628f72f13279d869b4fb4e1c2afce0d6a7a8
shuall <shualloret@gmail.com>
2017-05-13 17:04:49 -0400

    bin cleanup, uniglyph
    removed some old unused scripts
    added a new script that selects unicode chars

---
+++ bin/uniglyph.sh
@@ -0,0 +1,54 @@
+#!/bin/sh
+
+uniglyphs="$HOME/.config/uniglyphs.txt"
+arg0=$0
+
+gen() {
+	nameslist='http://unicode.org/Public/9.0.0/ucd/NamesList.txt'
+	
+	uniglyph() {
+		case $1 in
+		00[0-7][0-9A-F])
+			n=$(printf "%02x" 0x$1)
+			g=$(printf "\x$n") 
+			;;
+		[0-7A-F][0-7A-F][0-7A-F][0-7A-F])
+			g=$(printf "\u$1") 
+			;;
+		*)
+			n=$(printf "%08x" 0x$1)
+			g=$(printf "\U$n") 
+			;;
+		esac
+	#	g=$(echo "\u$1");
+		shift;
+		echo "$g" $@;
+	}
+	
+	curl "$nameslist" \
+		|grep "^[0-9A-F]"  |grep -v "<*>" \
+		|(while read l; do uniglyph $l; done) >$uniglyphs
+}
+
+sel() {
+	if [ ! -r $uniglyphs ]; then
+		echo "ERROR: uniglyphs file must be generated first with" >&2
+		echo "	$(basename $arg0) gen" >&2
+		exit
+	fi
+
+	cat $uniglyphs |dmenu -b -i -l 10 -fn "monospace:size=16" |cut -d' ' -f1 -z
+}
+
+case $1 in
+gen)
+	gen
+	;;
+sel)
+	sel
+	;;
+*)
+	sel |xsel -b
+	;;
+esac
+
